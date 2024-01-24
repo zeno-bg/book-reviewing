2233e3a97403d7474771291c4293083bdf20def9
Georgi Zenopyan <georgi@zenformatic.com>
2024-01-24 14:55:30 +0200

    exception handling

--- exceptions.py
+++ exceptions.py
@@ -1,3 +1,9 @@
+
+import inspect
+
+from decorator import decorate
+
+
 class BaseServiceException(Exception):
     detail: str
 
@@ -7,3 +13,17 @@ class BaseServiceException(Exception):
 
 class ObjectNotFoundException(BaseServiceException):
     pass
+
+
+class DatabaseException(Exception):
+    pass
+
+
+async def _database_exception_wrapper(func, *args, **kwargs):
+    try:
+        return await func(*args, **kwargs)
+    except Exception as e:
+        raise DatabaseException(e)
+
+def database_exception_wrapper(func):
+    return decorate(func, _database_exception_wrapper)
--- main.py
+++ main.py
@@ -1,10 +1,44 @@
 import uvicorn as uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.encoders import jsonable_encoder
+from fastapi.exception_handlers import http_exception_handler, request_validation_exception_handler
+from fastapi.exceptions import RequestValidationError, HTTPException
+from pydantic import ValidationError
+from starlette.responses import JSONResponse
+from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
+from exceptions import ObjectNotFoundException, DatabaseException
 from routers.users import router as users_router
 
 app = FastAPI(root_path="/api/v1")
 app.include_router(users_router, tags=["Users"], prefix="/users")
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exception: RequestValidationError):
+    print("error4e!")
+    print(exception.errors())
+
+    return await request_validation_exception_handler(request, exception)
+    #return JSONResponse(status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+    #                    content=jsonable_encoder({"errors": exception.errors()}))
+    # TODO: logging
+
+
+@app.exception_handler(ObjectNotFoundException)
+async def custom_http_exception_handler(request: Request, exception: ObjectNotFoundException):
+    print("NOT FOUND4E!")
+    raise HTTPException(status_code=404, detail=exception.detail)
+
+
+@app.exception_handler(DatabaseException)
+async def database_exception_handler(request: Request, exception: DatabaseException):
+    print("DATABASE EXCEPTION!")
+    print(exception)
+    raise HTTPException(status_code=500, detail="Database exception occurred!")
+
+
+
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
